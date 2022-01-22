79538e4d92655443cd38ad4a507af0b25819478e
Edwino Stein <edwino.stein@gmail.com>
2022-01-22 18:13:08 -0400

    Add library CMake subproject
    Issue #16

---
+++ library/include/elrond_runtime_library_types.hpp
@@ -0,0 +1,8 @@
+#ifndef ELROND_RUNTIME_LIBRARY_TYPES_HPP
+    #define ELROND_RUNTIME_LIBRARY_TYPES_HPP
+
+    #include <elrond.hpp>
+    #include <elrond_platform_runtime.hpp>
+    #include <elrond_common_runtime.hpp>
+
+#endif
