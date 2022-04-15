0adb4e593fc7b232fb76b648dab4569abebce493
Edwino Stein <edwino.stein@gmail.com>
2022-04-15 14:02:28 -0400

    Add application::ModuleFactoryPool class
    Issue #15

--- base_runtime/include/elrond_base_runtime_types.hpp
+++ base_runtime/include/elrond_base_runtime_types.hpp
@@ -5,4 +5,19 @@
     #include <elrond_platform_runtime.hpp>
     #include <elrond_common_runtime.hpp>
 
+    #include <map>
+
+    namespace elrond
+    {
+        namespace platform
+        {
+            using FactoriesM = std::map<std::string, elrond::platform::FactoryAdapterP>;
+        }
+
+        namespace application
+        {
+            class ModuleFactoryPool;
+        }
+    }
+
 #endif
--- base_runtime/include/elrond_runtime.hpp
+++ base_runtime/include/elrond_runtime.hpp
@@ -1,6 +1,6 @@
 #ifndef ELROND_BASE_RUNTIME_HPP
     #define ELROND_BASE_RUNTIME_HPP
 
-    #include "elrond_base_runtime_types.hpp"
+    #include "application/ModuleFactoryPool.ipp"
 
 #endif
